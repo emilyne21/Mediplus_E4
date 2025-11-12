34366f17f41e3367a0b391687e7c6219bfe1a162
Amira Perez del Aguila <aepdam21@gmail.com>
2025-11-12 01:05:24 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Importar el CSS de Tailwind para que Vite lo procese
+import './styles/tailwind.css';
+
+// Este archivo asegura que el CSS se compile cuando se ejecuta el servidor de desarrollo
+
